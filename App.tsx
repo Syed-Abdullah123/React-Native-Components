8ea151a788ade1215b2a961630f79e106e6ab09d
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-03-05 12:23:29 +0500

    calling the features component to display the flip card

--- App.tsx
+++ App.tsx
@@ -1,32 +1,36 @@
-import React from 'react'
-import { View, StyleSheet } from 'react-native'
-import { StatusBar } from 'expo-status-bar'
-import { useFonts } from 'expo-font'
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { useFonts } from "expo-font";
 // import GestureTopTabNavigator from './src/components/TopBarGestureBased'
 // import GestureBottomTabNavigator from './src/components/BottomTabGestureBased'
-import SpinningWheel from './src/components/SpinningWheelComponent'
+import SpinningWheel from "./src/components/SpinningWheelComponent";
+import ThreeDViewer from "./src/components/ThreeDViewer";
+import HomeScreen from "./src/components/HomeScreen";
+import FeaturesComponent from "./src/components/FeaturesComponent";
 
 export default function App() {
   const [loaded] = useFonts({
-    'Lexend-Regular': require('./assets/fonts/Lexend-Regular.ttf'),
-    'Lexend-Bold': require('./assets/fonts/Lexend-Bold.ttf'),
-  })
+    "Lexend-Regular": require("./assets/fonts/Lexend-Regular.ttf"),
+    "Lexend-Bold": require("./assets/fonts/Lexend-Bold.ttf"),
+  });
 
   if (!loaded) {
-    return null
+    return null;
   }
 
   return (
     <View style={styles.container}>
-      <SpinningWheel />
-      <StatusBar style="light"  />
+      {/* <HomeScreen /> */}
+      <FeaturesComponent />
+      <StatusBar style="light" />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
-})
+});
