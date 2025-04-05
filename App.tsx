f7a881f066fe10cae5b719532e6519c5f9156f22
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-04-05 17:00:38 +0500

    displaying all the custom loader at once

--- App.tsx
+++ App.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import { View, StyleSheet } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { useFonts } from "expo-font";
-import CustomLoaderOne from "./src/components/Loaders/CustomLoaderOne";
+import GamepadCircleLoader from "./src/components/Loaders/GamepadCircleLoader";
+import BatteryLoader from "./src/components/Loaders/BatteryLoader";
+import MorphingDotsLoader from "./src/components/Loaders/MorphingDotsLoader";
 // import GestureTopTabNavigator from './src/components/TopBarGestureBased'
 // import GestureBottomTabNavigator from './src/components/BottomTabGestureBased'
 // import SpinningWheel from "./src/components/SpinningWheelComponent";
@@ -22,7 +24,9 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <CustomLoaderOne />
+      <BatteryLoader />
+      <GamepadCircleLoader />
+      <MorphingDotsLoader />
       <StatusBar style="light" />
     </View>
   );
