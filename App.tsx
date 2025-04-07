a9357fb224c4c1d8d8e1d87fe2c7d4063a934787
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-04-07 21:40:37 +0500

    removed unnecessary code from the entry point

--- App.tsx
+++ App.tsx
@@ -5,12 +5,6 @@ import { useFonts } from "expo-font";
 import GamepadCircleLoader from "./src/components/Loaders/GamepadCircleLoader";
 import BatteryLoader from "./src/components/Loaders/BatteryLoader";
 import MorphingDotsLoader from "./src/components/Loaders/MorphingDotsLoader";
-// import GestureTopTabNavigator from './src/components/TopBarGestureBased'
-// import GestureBottomTabNavigator from './src/components/BottomTabGestureBased'
-// import SpinningWheel from "./src/components/SpinningWheelComponent";
-// import ThreeDViewer from "./src/components/ThreeDViewer";
-// import HomeScreen from "./src/components/HomeScreen";
-// import FeaturesComponent from "./src/components/FeaturesComponent";
 
 export default function App() {
   const [loaded] = useFonts({
@@ -24,8 +18,8 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <BatteryLoader />
-      <GamepadCircleLoader />
+      {/* <BatteryLoader /> */}
+      {/* <GamepadCircleLoader /> */}
       <MorphingDotsLoader />
       <StatusBar style="light" />
     </View>
